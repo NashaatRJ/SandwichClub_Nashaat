bcf899497a8d9955a22a454696eebfaedbe68a91
NashaatRJ <nashaat.r.jouda@hotmail.com>
2018-09-13 15:45:41 +0300

    Commiting source code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sandwich Club
+
+ **Project name:** Sandwich Club
+
+ **Track:** Android Advanced (AND)
+ 
+  Udacity Android Nanodegree Project
+
+## Starter code
+[Sandwich Club starter code](https://github.com/udacity/sandwich-club-starter-code)
---
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -0,0 +1,111 @@
+package com.udacity.sandwichclub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.model.Sandwich;
+import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.nio.charset.Charset;
+import java.util.List;
+public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_POSITION = "extra_position";
+    private static final int DEFAULT_POSITION = -1;
+
+    private ImageView pic;
+    private TextView ingred;
+    private TextView alsoKnown_text;
+    private TextView alsoKnown;
+    private TextView desc;
+    private TextView orgT;
+    private TextView org;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        pic = findViewById(R.id.image_iv);
+        orgT = findViewById(R.id.origin_label);
+        org = findViewById(R.id.origin_tv);
+        alsoKnown_text = findViewById(R.id.also_known_label);
+        alsoKnown = findViewById(R.id.also_known_tv);
+        ingred = findViewById(R.id.ingredients_tv);
+        desc = findViewById(R.id.description_tv);
+
+        Intent intent = getIntent();
+        if (intent == null) {
+            closeOnError();
+        }
+
+        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        if (position == DEFAULT_POSITION)
+        {
+            // EXTRA_POSITION not found in intent
+            closeOnError();
+            return;
+        }
+        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        String json = sandwiches[position];
+        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        if (sandwich == null) {
+            // Sandwich data unavailable
+            closeOnError();
+            return;}
+
+        setTitle(sandwich.getMainName());
+        populateUI(sandwich);
+    }
+    private void closeOnError() {
+        finish();
+        Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void populateUI(Sandwich sandwich) {
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(pic);
+
+        if (sandwich.getPlaceOfOrigin().isEmpty()) {
+            // origin.setText(R.string.detail_error_message);
+            // if we don't have origin, hide it.
+            orgT.setVisibility(View.GONE);
+            org.setVisibility(View.GONE);
+        } else {
+            org.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        if (sandwich.getAlsoKnownAs().isEmpty()) {
+            // alsoKnown.setText(R.string.detail_error_message);
+            // if we don't have aka, hide it.
+            alsoKnown_text.setVisibility(View.GONE);
+            alsoKnown.setVisibility(View.GONE);
+        } else {
+            List<String> aka = sandwich.getAlsoKnownAs();
+            String aka_str = TextUtils.join(", ", aka);
+            alsoKnown.setText(aka_str);
+        }
+        desc.setText(sandwich.getDescription());
+        List<String> ing = sandwich.getIngredients();
+        String ing_str = TextUtils.join(", ", ing);
+        ingred.setText(ing_str);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
